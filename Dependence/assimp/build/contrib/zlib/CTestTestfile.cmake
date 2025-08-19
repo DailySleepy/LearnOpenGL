1033a7aeb72d087dc92b24f1f22272b9735899a9
wanwan <dailysleepy@qq.com>
2025-08-20 01:51:39 +0800

    移除不必要的依赖, 增加必要的依赖

--- Dependence/assimp/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/wanwan/Documents/Visual Studio 2022/projects/LearnOpenGL/Dependence/assimp/contrib/zlib
-# Build directory: C:/Users/wanwan/Documents/Visual Studio 2022/projects/LearnOpenGL/Dependence/assimp/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
